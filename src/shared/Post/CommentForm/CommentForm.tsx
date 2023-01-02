0f11658fc92b64167446b6f7da0001c6b6f55c77
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2023-01-03 02:10:43 +0300

    Сохранил состояние формы и валидацию поля. Вывел ошибки в правильный момент времени.

--- src/shared/Post/CommentForm/CommentForm.tsx
+++ src/shared/Post/CommentForm/CommentForm.tsx
@@ -6,22 +6,44 @@ import { commentUpdate } from "../../../store/comments/actions";
 
 export function CommentForm() {
   const value = useSelector<RootState, string>((state) => state.comments.commentText);
+  const [touched, setTouched] = React.useState(false);
+  // const [valueError, setValueError] = React.useState("");
   const dispatch = useDispatch();
 
   const refTextarea = React.useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    setTouched(true);
+
+    const isFormValid = !validateValue();
+
+    if (!isFormValid) return;
+
+    alert("Форма отправлена");
+  }
 
   function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
     dispatch(commentUpdate(event.target.value));
   }
 
-  function handleSubmit(event: FormEvent) {
-    event.preventDefault();
-    console.log(value);
+  function validateValue() {
+    if (value.length <= 3) return "Введите больше 3-х символов";
+    return "";
   }
 
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <textarea className={styles.input} value={value} onChange={handleChange} ref={refTextarea} />
+      <textarea
+        className={styles.input}
+        value={value}
+        onChange={handleChange}
+        ref={refTextarea}
+        aria-invalid={validateValue() ? "true" : undefined}
+      />
+      {touched && validateValue() && (
+        <div className={styles.validateMessage}>{validateValue()}</div>
+      )}
       <button type="submit" className={styles.button}>
         Комментировать
       </button>
