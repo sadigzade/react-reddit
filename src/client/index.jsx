d4fc2117411656bd605fdaa744a13d1e2aa12604
Хикмет Садигзаде <sadigzade.hi@mail.ru>
2023-01-05 00:51:04 +0300

    Подключил React-router-dom

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "../App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-window.addEventListener('load', () => {
-  ReactDOM.hydrate(<App />, document.getElementById('react_root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+window.addEventListener("load", () => {
+  ReactDOM.hydrate(<RouterProvider router={router} />, document.getElementById("react_root"));
 });
